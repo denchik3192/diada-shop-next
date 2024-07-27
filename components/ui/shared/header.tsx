ece2560e268fb6f66e789235465e600e8da7db23
DENIS <den465341@gmail.com>
2024-07-27 22:01:19 +0300

    init: satat project add all deps and libs

---
+++ components/ui/shared/header.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container } from './container';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { Button } from '../button';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+
+type Props = { className?: string };
+
+const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn('border border-b', className)}>
+      <Container className="flex items-center justify-between py-8">
+        <div>
+          <Image src={'/next.svg'} alt="Logo" width={100} height={100} />
+        </div>
+        <div className="flex items-center">
+          <Button className="mr-4 flex items-center gap-1" variant={'outline'}>
+            <User size={16} />
+            Войти
+          </Button>
+          <div>
+            <Button className="group relative">
+              <b>500 ₽</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
